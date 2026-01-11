b4787c19bbdb5a7c12cae652c820609a84831cf3
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:28:25 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "advanced_threat_protections" {
+  description = "All advanced_threat_protection resources"
+  value       = azurerm_advanced_threat_protection.advanced_threat_protections
+}
+
