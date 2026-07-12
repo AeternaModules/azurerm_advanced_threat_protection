6c1ab37886e370827c1bc4f457e4bfda41e12790
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "advanced_threat_protections" {
-  description = "All advanced_threat_protection resources"
-  value       = azurerm_advanced_threat_protection.advanced_threat_protections
-}
 output "advanced_threat_protections_enabled" {
-  description = "List of enabled values across all advanced_threat_protections"
-  value       = [for k, v in azurerm_advanced_threat_protection.advanced_threat_protections : v.enabled]
+  description = "Map of enabled values across all advanced_threat_protections, keyed the same as var.advanced_threat_protections"
+  value       = { for k, v in azurerm_advanced_threat_protection.advanced_threat_protections : k => v.enabled }
 }
 output "advanced_threat_protections_target_resource_id" {
-  description = "List of target_resource_id values across all advanced_threat_protections"
-  value       = [for k, v in azurerm_advanced_threat_protection.advanced_threat_protections : v.target_resource_id]
+  description = "Map of target_resource_id values across all advanced_threat_protections, keyed the same as var.advanced_threat_protections"
+  value       = { for k, v in azurerm_advanced_threat_protection.advanced_threat_protections : k => v.target_resource_id }
 }
 
